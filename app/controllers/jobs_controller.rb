e9f7a6b69d813ac41414ff019faf83ace5126575
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-01 03:58:00 +0700

    update def index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,8 @@
 class JobsController < ApplicationController
   before_action :find_job, only: [:show, :edit, :update, :destroy]
+
   def index
+    @jobs = Job.all.order("created_at DESC")
   end
 
   def show
